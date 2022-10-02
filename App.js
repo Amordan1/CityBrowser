18be328c6ccfec7b566f4e7e04fa7de54a94c99d
Reza Majlessi <rm634127@southeast.edu>
2022-10-01 21:29:25 -0500

    initial commit

--- App.js
+++ App.js
@@ -1,98 +1,90 @@
-import React, { Component } from 'react';
-import {
-  StyleSheet,
-  View,
-  //ListView,
-  Image,
-  Text,
-  FlatList
-} from 'react-native';
-//import ListView from 'deprecated-react-native-listview';
-import data from './data.json';
+import * as React from 'react';
+import { StyleSheet, View, Image } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
-const chicago = require('./images/chicago.png');
+const art = require('./assets/art.png');
+const mile = require('./assets/mile.png');
+const pier = require('./assets/pier.png');
+const water = require('./assets/water.png');
+const willis = require('./assets/willis.png');
 
-export default class App extends React.Component {
-  renderRow({item}) {
-    return (
-      <View style={styles.row}>
-        <View style={styles.iconContainer}>
-          <Image source={chicago} style={styles.icon} />
-        </View>
-        <View style={styles.info}>
-          <Text style={styles.items}>{item.items} Items</Text>
-          <Text style={styles.address}>{item.address}</Text>
-          <Text style={styles.url}>{item.modifiedUrl}</Text>
-        </View>
-      </View>
-    );
-  }
+function HomeScreen() {
+  return (
+    <View style={styles.mainContainer}>
+      <Image style={styles.image} source={art}></Image>
+    </View>
+  );
+}
+function MileScreen() {
+  return (
+    <View style={styles.mainContainer}>
+      <Image style={styles.image} source={mile}></Image>
+    </View>
+  );
+}
+function PierScreen() {
+  return (
+    <View style={styles.mainContainer}>
+      <Image style={styles.image} source={pier}></Image>
+    </View>
+  );
+}
+function WaterScreen() {
+  return (
+    <View style={styles.mainContainer}>
+      <Image style={styles.image} source={water}></Image>
+    </View>
+  );
+}
+function WillisScreen() {
+  return (
+    <View style={styles.mainContainer}>
+      <Image style={styles.image} source={willis}></Image>
+    </View>
+  );
+}
 
-  render() {
-    return (
-      <View style={styles.mainContainer}>
-        <Text style={styles.title}>City Guide</Text>
-        <FlatList data={data} renderItem={this.renderRow} /> 
-      </View>
-    );
-  }
+const Drawer = createDrawerNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator>
+        <Drawer.Screen name="Home" component={HomeScreen}
+        options={{ headerStyle: { backgroundColor: "#fff" },
+        headerTintColor: "#000",
+        title: "Art Institute of Chicago"}} />
+        <Drawer.Screen name="Mile" component={MileScreen}
+        options={{ headerStyle: { backgroundColor: "#fff" },
+        headerTintColor: "#000",
+        title: "Magnificent Mile"}} />
+        <Drawer.Screen name="Pier" component={PierScreen}
+        options={{ headerStyle: { backgroundColor: "#fff" },
+        headerTintColor: "#000",
+        title: "Navy Pier"}} />
+        <Drawer.Screen name="Water" component={WaterScreen}
+        options={{ headerStyle: { backgroundColor: "#fff" },
+        headerTintColor: "#000",
+        title: "Water Tower"}} />
+        <Drawer.Screen name="Willis" component={WillisScreen}
+        options={{ headerStyle: { backgroundColor: "#fff" },
+        headerTintColor: "#000",
+        title: "Willis Tower"}} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
 }
 
 const styles = StyleSheet.create({
   mainContainer: {
     flex: 1,
-    backgroundColor: '#ede2e0',
-  },
-  title: {
-    backgroundColor: '#1c7da5',
-    color: '#ede2e0',
-    fontSize: 18,
-    fontWeight: 'bold',
-    padding: 10,
-    paddingTop: 40,
-    textAlign: 'center',
-  },
-  row: {
-    borderColor: '#7b7983',
-    borderBottomWidth: 1,
-    flexDirection: 'row',
-    marginLeft: 10,
-    marginRight: 10,
-    paddingTop: 20,
-    paddingBottom: 20,
-  },
-  iconContainer: {
     alignItems: 'center',
-    backgroundColor: '#1f3243',
-    borderColor: '#1f3243',
-    borderRadius: 25,
-    borderWidth: 1,
     justifyContent: 'center',
-    height: 70,
-    width: 70,
+    backgroundColor: '#fff'
   },
-  icon: {
-    height: 45,
-    width: 45,
-  },
-  info: {
-    flex: 1,
-    paddingLeft: 25,
-    paddingRight: 25,
-  },
-  items: {
-    color: '#122737',
-    fontWeight: 'bold',
-    fontSize: 18,
-    marginBottom: 5,
-  },
-  address: {
-    color: '#3c4f62',
-    fontSize: 16,
-  },
-  url: {
-    color: '#3c4f62',
-    fontSize: 16,
-    fontStyle: 'italic',
+  image: {
+    height: 480,
+    width: 320
   }
 });
