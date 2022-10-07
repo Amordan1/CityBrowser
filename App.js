f16061e349f8c64f8e4958fe8ae2d37cd908cbf7
Reza Majlessi <rm634127@southeast.edu>
2022-10-07 18:38:08 -0500

    Initial Commit

--- App.js
+++ App.js
@@ -1,18 +1,34 @@
 import * as React from 'react';
-import { StyleSheet, View, Image } from 'react-native';
+import { StyleSheet, View, Image, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
+import Button from './Button';
+import * as WebBrowser from 'expo-web-browser';
 
 const art = require('./assets/art.png');
 const mile = require('./assets/mile.png');
 const pier = require('./assets/pier.png');
 const water = require('./assets/water.png');
 const willis = require('./assets/willis.png');
+const getInfo = "More Information";
+const artURL = "https://www.artic.edu/";
+const mileURL = "https://www.themagnificentmile.com/";
+const pierURL = "https://navypier.org/";
+const waterURL = "https://www.chicago.gov/city/en/depts/dca/supp_info/city_gallery_in_thehistoricwatertower.html";
+const willisURL = "https://www.willistower.com/";
+
 
 function HomeScreen() {
   return (
     <View style={styles.mainContainer}>
       <Image style={styles.image} source={art}></Image>
+      <Button
+      info
+      onPress={() => handleButtonPress(artURL)}
+      style={styles.button}
+    >
+      <Text>{getInfo}</Text>
+    </Button>
     </View>
   );
 }
@@ -20,6 +36,13 @@ function MileScreen() {
   return (
     <View style={styles.mainContainer}>
       <Image style={styles.image} source={mile}></Image>
+      <Button
+      info
+      onPress={() => handleButtonPress(mileURL)}
+      style={styles.button}
+    >
+      <Text>{getInfo}</Text>
+    </Button>
     </View>
   );
 }
@@ -27,6 +50,13 @@ function PierScreen() {
   return (
     <View style={styles.mainContainer}>
       <Image style={styles.image} source={pier}></Image>
+      <Button
+      info
+      onPress={() => handleButtonPress(pierURL)}
+      style={styles.button}
+    >
+      <Text>{getInfo}</Text>
+    </Button>
     </View>
   );
 }
@@ -34,6 +64,13 @@ function WaterScreen() {
   return (
     <View style={styles.mainContainer}>
       <Image style={styles.image} source={water}></Image>
+      <Button
+      info
+      onPress={() => handleButtonPress(waterURL)}
+      style={styles.button}
+    >
+      <Text>{getInfo}</Text>
+    </Button>
     </View>
   );
 }
@@ -41,13 +78,25 @@ function WillisScreen() {
   return (
     <View style={styles.mainContainer}>
       <Image style={styles.image} source={willis}></Image>
+      <Button
+      info
+      onPress={() => handleButtonPress(willisURL)}
+      style={styles.button}
+    >
+      <Text>{getInfo}</Text>
+    </Button>
     </View>
   );
+}
+
+function handleButtonPress(button) {
+  WebBrowser.openBrowserAsync(button);
 }
 
 const Drawer = createDrawerNavigator();
 
 export default function App() {
+
   return (
     <NavigationContainer>
       <Drawer.Navigator>
@@ -86,5 +135,8 @@ const styles = StyleSheet.create({
   image: {
     height: 480,
     width: 320
-  }
+  },
+  button: {
+    margin: 10,
+  },
 });
